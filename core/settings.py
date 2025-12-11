8862d281cdf08ff5722a5508e4af0900cd580945
Mukul Badge <badgemukul36@gmail.com>
2025-12-11 16:32:47 +0530

    defult database use

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_^3bjp_=6$*oap8-l3c9^2klcri3#p1)4fx%ot7#4)m3b&0j1*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "school",
-        "USER": "mukul",
-        "PASSWORD": "Badge@2003",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # "ENGINE": "django.db.backends.postgresql",
+        # "NAME": "school",
+        # "USER": "mukul",
+        # "PASSWORD": "Badge@2003",
+        # "HOST": "127.0.0.1",
+        # "PORT": "5432",
     }
 }
 
